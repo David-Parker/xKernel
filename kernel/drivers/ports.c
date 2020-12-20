418cdbcb9ef6e015edfd39e7a4122396402d5500
ubuntu 18.04 64-bit <davidgparker123@utexas.edu>
2020-12-19 22:28:17 -0800

    writing I/O ports

---
+++ kernel/drivers/ports.c
@@ -0,0 +1,27 @@
+#include <kernel/drivers/ports.h>
+
+unsigned char read_port_byte (unsigned short port)
+{
+    unsigned char result;
+    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+
+    return result;
+}
+
+void write_port_byte (unsigned short port, unsigned char data)
+{
+    __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+}
+
+unsigned short read_port_word (unsigned short port)
+{
+    unsigned short result;
+
+    __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
+    return result;
+}
+
+void write_port_word (unsigned short port, unsigned short data)
+{
+    __asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
+}
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,6 +2,7 @@
 
 void main() {
     kprintf("Hello %s, this is a test of printf! %d", "World", 42);
+    // kprintf("Sup\n");
 
     while(1);
 }
